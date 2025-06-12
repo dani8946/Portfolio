38e21be08d47f2ea454cb908ce8be4b48daddf6c
dani8946 <brahmadanielkr@gmail.com>
2025-06-12 22:45:20 +0530

    First Commit

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import python from "./tech/python.svg"
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import sql from "./tech/sql.svg"
+import c from "./tech/C.png"
+import cs from "./tech/Sharp.jpg"
+
+import NIELIT from "./company/NIELIT.jpg"
+import IGDTUW from "./company/IGDTUW.png";
+import IIT from "./company/IIT.png";
+
+import wholeset from "./wholeset.png";
+import swc from "./SWC.png";
+import MRS from "./MRS.png";
+import WBNS from "./WBNS.png";
+import LC from "./LC.png";
+import AR from "./AR.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  nodejs,
+  python,
+  reactjs,
+  tailwind,
+  threejs,
+  MRS,
+  IGDTUW,
+  IIT,
+  NIELIT,
+  sql,
+  c,
+  cs,wholeset,
+  swc,
+  WBNS,
+  LC,AR
+};
---
+++ src/constants/index.js
@@ -0,0 +1,285 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  git,
+  IGDTUW,
+  IIT,
+  swc,
+  threejs,
+  NIELIT,
+  python,
+  sql,
+  c,
+  cs, wholeset,
+  MRS,
+  WBNS,
+  LC,
+  AR
+} from "../assets";
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "AI/ML Model Developer",
+    icon: web,
+  },
+  {
+    title: "DL Model Developer  ",
+    icon: mobile,
+  },
+  {
+    title: "Python Developer",
+    icon: backend,
+  },
+  {
+    title: "Data Scienctist",
+    icon: creator,
+  },
+];
+
+const technologies = [
+   {
+    name: "python",
+    icon: python,
+  },
+  {
+    name: "sql",
+    icon: sql,
+  },
+  {
+    name: "C",
+    icon: c,
+  },
+  {
+    name: "C#",
+    icon: cs,
+  },
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+   {
+    name: "Node JS",
+    icon: nodejs,
+  },
+   {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+ 
+];
+
+const experiences = [
+  {
+    title: "AR Intern (Online)",
+    company_name: "IGDTUW, Delhi",
+    icon:IGDTUW,
+    iconBg: "#383E56",
+    date: "5th June - 18th July, 2023",
+    points: [
+      "Developed an Android application to project 3D model of the target object along with brief description.",
+      "Gained hands-on experience in developing AR based websites by using A-Frame.",
+      "Participated in weekly reviews and presentations, enhancing communication skills and technical documentation practices.",
+      "Participated in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "AR Intern (On-Site)",
+    company_name: "IIT Guwahati",
+    icon: IIT,
+    iconBg: "#E6DEDD",
+    date: "22nd June - 22nd July, 2023",
+    points: [
+      "Worked on Avatar Mirroring using Microsoft Kinect, enabling real-time motion tracking and mapping of human body movements onto a 3D avatar in Unity 3D.",
+      "Gained hands-on experience in Unity 3D and C# scripting, contributing to interactive development workflows and enhancing real-time simulation accuracy.",
+      "Assisted in creating a demonstrable prototype for body movement capture and avatar synchronization, supporting ongoing research in human-computer interaction.",
+      "Prepared lecture materials and tutorials on Unity 3D for students enrolled in related coursework, helping them understand the fundamentals and project development pipeline.",
+    ],
+  },
+  {
+    title: "AI/ML Intern(WBL)",
+    company_name: "NIELIT Kokrajhar EC",
+    icon: NIELIT,
+    iconBg: "#383E56",
+    date: "1st Aug 2024 - Present",
+    points: [
+      "Developed a real-time Emotion Detection system using CNN to classify human facial expressions, contributing to research in affective computing and intelligent systems.",
+      "Conducted 5-day AI/ML workshop series under PM-Shri Programme across multiple educational institutions, delivering technical sessions and demonstrations.",
+      "Conducted a 2-week Cyber Security Awareness workshop exclusively for police personnel, focusing on digital safety, threat identification, and preventive measures.",
+      "Gained teaching experience by mentoring students at NIELIT in Python and JavaScript, delivering hands-on training and guiding them through real-world coding exercises and mini-projects.",
+    ],
+  },
+];
+
+
+const projects = [
+  {
+    name: "Chrome Extension for Optimization",
+    description:
+      "A dynamic browser extension that intelligently adapts webpage rendering based on the user’s network speed, optimizing performance and enhancing overall browsing experience and satisfaction.",
+    tags: [
+      {
+        name: "Python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "JavaScript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Optimization",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: WBNS,
+    source_code_link: "https://github.com/dani8946/A-Dynamic-Approach-of-Web-Pages--Rendering-based-on-Network-Speed",
+  },
+  {
+    name: "Emotion Detection Using CNN",
+    description:
+      "A high-performance CNN-based Emotion Detection model that accurately identifies user emotions with an impressive accuracy of 89.45%, enabling intelligent and responsive human-computer interactions.",
+    tags: [
+      {
+        name: "Python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CNN",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Deep Learning",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: wholeset,
+    source_code_link: "https://github.com/dani8946/Emotion-Detection-using-CNN",
+  },
+  {
+    name: "Lung Cancer Risk Prediction",
+    description:
+      "A machine learning model that effectively classifies risk levels into High, Medium, and Low, achieving a K-fold cross-validation accuracy of 89.57% for reliable and data-driven risk assessment. ",
+    tags: [
+      {
+        name: "Python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "SciKit-Learn",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Numpy",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: LC,
+    source_code_link: "https://github.com/dani8946/LungCancerRiskPrediction/blob/main/ML_project.ipynb",
+  },
+  {
+    name: "Smart Waste Classifier ",
+    description:
+      "A MobileNetV2 powered Smart Waste Classifier, accurately categorizes waste into six distinct types — plastic, paper, metal, trash, glass, and cardboard enabling automated eco-friendly waste sorting and promoting sustainable environment practices.",
+    tags: [
+      {
+        name: "Python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "TensorflowKeras",
+        color: "green-text-gradient",
+      },
+      {
+        name: "MobileNetV2",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: swc,
+    source_code_link: "https://github.com/dani8946/SmartWasteClassifier",
+  },
+  {
+    name: "Content Based Movie Recommendation System",
+    description:
+      "A system that recommends the movie based on the content of the movie demonstrating the use of Unsupervised Learning.",
+    tags: [
+      {
+        name: "Python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Unsupervised",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Cosine-Similarity",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: MRS,
+    source_code_link: "https://github.com/dani8946/ContentBasedMovieRecommendationSystem",
+  },
+  {
+    name: "AR Application for Education",
+    description:
+      "An interactive application that dynamically renders 3D models of target images along with concise object descriptions, enhancing user engagement and visual understanding through immersive technology.",
+    tags: [
+      {
+        name: "C#",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Unity3D",
+        color: "green-text-gradient",
+      },
+      {
+        name: "VuforiaEngine",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: AR,
+    source_code_link: "https://github.com/dani8946/AR-In-Education",
+  },
+];
+
+export { services, technologies, experiences,  projects };
